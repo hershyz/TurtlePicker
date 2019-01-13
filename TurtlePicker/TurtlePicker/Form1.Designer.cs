2d49845384402db0524eb500b22c8719fda9a9cd
Hrishikesh Bagalkote <bagalkote.family@outlook.com>
2019-01-13 13:40:58 -0500

    Add files via upload

---
+++ TurtlePicker/TurtlePicker/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace TurtlePicker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.timer = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.1F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(10, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(279, 55);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "TurtlePicker";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(100)))), ((int)(((byte)(100)))));
+            this.label2.Location = new System.Drawing.Point(22, 82);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(158, 32);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "By Hershyz";
+            // 
+            // timer
+            // 
+            this.timer.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.timer.ForeColor = System.Drawing.Color.Green;
+            this.timer.Location = new System.Drawing.Point(131, 185);
+            this.timer.Name = "timer";
+            this.timer.Size = new System.Drawing.Size(449, 83);
+            this.timer.TabIndex = 4;
+            this.timer.Text = "45";
+            this.timer.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.timer.Click += new System.EventHandler(this.timer_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.ForeColor = System.Drawing.Color.White;
+            this.richTextBox1.Location = new System.Drawing.Point(28, 311);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(684, 357);
+            this.richTextBox1.TabIndex = 5;
+            this.richTextBox1.Text = "";
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Interval = 1;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.ClientSize = new System.Drawing.Size(724, 680);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.timer);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "TurtlePicker";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label timer;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Timer timer2;
+    }
+}
+
---
+++ TurtlePicker/TurtlePicker/Form1.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace TurtlePicker
+{
+
+    /*
+     * Directions: NW, NE, SW, SE
+     * Depths: Orange, Black, White, Corner
+     * 
+     * Directional Values:
+     * 1 = NW, 2 = NE, 3 = SW, 4 = SE
+     * 
+     * Depth Values:
+     * 1 = Orange, 2 = Black, 3 = White, 4 = Corner
+     * 
+     * Colors:
+     * 45-25 seconds - Green
+     * 45-11 seconds - Orange
+     * 10-0 seconds - Red
+     */
+
+    public partial class Form1 : Form
+    {
+        string[] directions = new string[] { "Northwest", "Northeast", "Southwest", "Southeast" };
+        string[] depths = new string[] { "Orange", "Black", "White", "Corner" };
+
+        Random random = new Random();
+        public int seconds = 45;
+        public int rounds = 1;
+
+        int R = 30;
+        int G = 30;
+        int B = 30;
+        bool maxRed = false;
+        bool maxGreen = false;
+        bool maxBlue = false;
+        bool changeColors = true;
+
+        public Form1()
+        {
+            InitializeComponent();
+            string initLocation = directions[random.Next(0, 4)].ToString() + ", " + depths[random.Next(0, 4)].ToString();
+            richTextBox1.AppendText(initLocation);
+        }
+
+        //When timer ticks:
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            update();
+        }
+
+        //Updates the timer label:
+        private void update()
+        {
+            timer.Text = seconds.ToString();
+            seconds--;
+
+            //Color Changing:
+            if (seconds <= 45 && seconds >= 25)
+            {
+                timer.ForeColor = Color.Green;
+            }
+
+            if (seconds <= 24 && seconds >= 11)
+            {
+                timer.ForeColor = Color.Orange;
+            }
+
+            if (seconds <= 10 && seconds >= 0)
+            {
+                timer.ForeColor = Color.Red;
+            }
+
+            //Timer Reset:
+            if (seconds == 0)
+            {
+                string direction = directions[random.Next(0, 4)];
+                string depth = depths[random.Next(0, 4)];
+
+                richTextBox1.AppendText("Round " + rounds.ToString() + ":  " + direction + ", " + depth + "\n");
+
+                rounds++;
+                seconds = 45;
+            }
+        }
+
+        //Color Changing:
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            label1.ForeColor = Color.FromArgb(R, G, B);
+
+            if (!changeColors)
+            {
+                timer2.Enabled = false;
+            }
+
+            updateColors();
+        }
+
+        private void updateColors()
+        {
+            if (R == 255)
+            {
+                maxRed = true;
+            }
+
+            if (B == 255)
+            {
+                maxBlue = true;
+            }
+
+            if (G == 255)
+            {
+                maxGreen = true;
+            }
+
+            generateNewColors();
+        }
+
+        private void generateNewColors()
+        {
+            if (!maxRed)
+            {
+                R++;
+                return;
+            }
+
+            if (!maxGreen)
+            {
+                G++;
+                return;
+            }
+
+            if (!maxBlue)
+            {
+                B++;
+                return;
+            }
+
+            if (maxRed && maxGreen && maxBlue)
+            {
+                R = 255;
+                G = 255;
+                B = 255;
+                changeColors = false;
+            }
+        }
+
+
+
+
+
+
+
+
+
+        //Ignore:
+        private void timer_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
